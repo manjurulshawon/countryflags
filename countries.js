3f4badb37adeef8fc35ef8132a5d34e344486593
manjurulshawon <shawon3154@.com>
2023-03-22 23:51:56 +0600

    first commit

---
+++ countries.js
@@ -0,0 +1,26 @@
+const loadCountries = () =>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data));
+}
+
+const displayCountries = countries =>{
+console.log(countries);
+const allcountriesHTML = countries.map(country => getcountryHTML(country));
+// console.log(allcountriesHTML);
+const container = document.getElementById('countries');
+container.innerHTML = allcountriesHTML.join('');
+
+}
+const getcountryHTML = country =>{
+    return`
+    <div class ="country">
+    <h2> ${country.name.common}</h2>
+    <img src="${country.flags.png}"/>
+
+    </div>
+    `
+}
+
+
+loadCountries();
